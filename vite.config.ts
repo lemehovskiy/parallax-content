4a8533ee4222474e3e8847e356d1cd04ffe693e5
Illia <lemehovskiy@gmail.com>
2023-12-18 13:39:23 +0200

    init release 2.0.0

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ParallaxContent',
+      fileName: 'parallaxContent',
+    },
+  },
+  plugins: [dts()],
+});
